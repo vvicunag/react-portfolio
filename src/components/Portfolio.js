23d832b68db543fa3b390b5055e8d20336d49d1c
vvicunag <vvicuna1@uc.cl>
2022-02-26 17:41:10 +0100

    add navbar

---
+++ src/components/Portfolio.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+const something = 1;
+
+export default function Portfolio() {
+    return (
+    <div>
+            <h1>Hello from Portfolio</h1>
+    <h2>My name is: {something}</h2>
+    </div>
+    )
+}
